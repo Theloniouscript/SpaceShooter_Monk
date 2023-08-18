f7a525ed05fbed722f0a3a2f109fcf4de1f3bd63
Lydia Pankratova <ecodeltaplan@gmail.com>
2023-08-18 19:18:47 +0300

    Spawn of different entities, powerup bonuses pickup feature, sounds for the projectile

---
+++ Assets/Scripts/PowerupStats.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class PowerupStats : Powerup
+    {
+        //public AudioSource audioPlayer;
+
+        public enum EffectType
+        {
+            AddAmmo, 
+            AddEnergy,
+            AddArmor
+        }
+
+        [SerializeField] private EffectType m_EffectType;
+        [SerializeField] private float m_Value;
+
+        
+
+        protected override void OnPickedUp(SpaceShip ship)
+        {
+            if (m_EffectType == EffectType.AddEnergy)
+                ship.AddEnergy((int) m_Value);
+                
+
+            if (m_EffectType == EffectType.AddAmmo)
+                ship.AddAmmo((int)m_Value);
+
+            if (m_EffectType == EffectType.AddArmor)
+                ship.AddArmor((int)m_Value);
+
+        }
+
+       /* private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(collision.gameObject.tag == "CollisionTag")
+            {
+                audioPlayer.Play();
+                Debug.Log("collision!!!");
+            }
+        }*/
+    }
+}
